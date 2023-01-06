ee0620b5e1abaea52d626bca1fc20c4cbb938da7
Junior Alves <momo.ju@hotmail.com>
2023-01-06 15:06:44 -0300

    FEAT begin project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TopoPadrao />
+    <ConteudoPadrao />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConteudoPadrao from './components/layouts/ConteudoPadrao.vue'
+import TopoPadrao from './components/layouts/TopoPadrao.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ConteudoPadrao,
+    TopoPadrao
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/layouts/ConteudoPadrao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+       
+        <home-padrao></home-padrao>
+        <publicar-vaga></publicar-vaga>
+    </div>
+</template>
+
+<script>
+import HomePadrao from '../views/HomePadrao.vue'
+import PublicarVaga from '../views/PublicarVaga.vue'
+export default {
+    name: 'ConteudoPadrao',
+    components: {
+        HomePadrao,
+        PublicarVaga
+    }
+}
+</script>
+
+<style></style>
